ddaf5f775e7ba74b077ae75f0da872e7da298403
Ubuntu2 <victor@ubuntu.(none)>
2013-01-14 12:54:09 -0800

    pictures added

--- amazonebooks/views.py
+++ amazonebooks/views.py
@@ -3,8 +3,9 @@ from formencode import Schema, validators
 from pyramid_simpleform import Form
 from pyramid.response import Response
 from pyramid_simpleform.renderers import FormRenderer
-from amazonproduct import API
-from amazonproduct.errors import *
+from lxml import etree
+from amazon.api import AmazonAPI
+
 
 
 class ModelSchema(Schema):
@@ -14,42 +15,68 @@ class ModelSchema(Schema):
     text = validators.MinLength(5, not_empty=True)
 
 
-@view_config(route_name='home', renderer='templates/template.pt')
-def my_view(request):
-    form = Form(request, schema=ModelSchema)
-    return dict(title = 'AmazoneBooks',renderer=FormRenderer(form),booklist=[])
-
 def removeNonAscii(s): 
     return "".join(filter(lambda x: ord(x)<128, s))
 
 
+
+@view_config(route_name='home', renderer='templates/template.pt')
+def my_view(request):
+    form = Form(request, schema=ModelSchema)
+    return dict(title = 'AmazoneBooks',renderer=FormRenderer(form),booklist=[],css_url= './templates/style.css')
+
+
+
 @view_config(route_name = 'search',renderer='templates/template.pt')
 def search_view(request):
-    try:
+    #try:
         if request.method == 'POST':
             form = Form(request, schema=ModelSchema)
             AWS_KEY = 'AKIAINFQGAWKSGFXLOVA'
             SECRET_KEY = '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'
             AssTag = 'victor073-20'
-            api = API(AWS_KEY,SECRET_KEY,'uk',AssTag)
+            api = AmazonAPI(AWS_KEY,SECRET_KEY,AssTag,region='UK')
             booklist = []
             i = 0
             if request.POST['text']:
-                for page in api.item_search('Books',Keywords=request.POST['text'],limit = 10):
-                    for book in page.Items.Item:
-                        booklist.append([])
-                        if hasattr(book.ItemAttributes, 'Author'):
-                            booklist[i] = removeNonAscii(book.ItemAttributes.Author.__str__()) + ' '
-                        if hasattr(book.ItemAttributes, 'Title'):
-                            booklist[i]+=removeNonAscii(book.ItemAttributes.Title.__str__()) + ' '
-                        if hasattr(book.ItemAttributes, 'Manufacturer'):
-                            booklist[i]+=removeNonAscii(book.ItemAttributes.Manufacturer.__str__())
-                        i+=1
+                books = api.search(Keywords=request.POST['text'], SearchIndex='Books')
+                for n,book in enumerate(books):
+                    booklist.append([])
+                    if hasattr(book.item.ItemAttributes, 'Author'):
+                        booklist[i].append('Author: ' + removeNonAscii(book.item.ItemAttributes.Author.__str__()))
+                    else:
+                        booklist[i].append('Author: -')
+                    if hasattr(book.item.ItemAttributes, 'Title'):
+                        booklist[i].append('Title: ' + removeNonAscii(book.item.ItemAttributes.Title.__str__()))
+                    else:
+                        booklist[i].append('Title: -')
+                    if hasattr(book.item.ItemAttributes, 'Publisher'):
+                        booklist[i].append("Publisher: " + removeNonAscii(book.item.ItemAttributes.Publisher.__str__()))
+                    else:
+                        booklist[i].append("Publisher: -")
+                    c = api.lookup(ItemId=book.asin)
+                    if hasattr(book.item, 'SmallImage'):
+                        booklist[i].append(removeNonAscii(book.item.SmallImage.URL.__str__()))
+                    else:
+                        booklist[i].append("-")
+                    if hasattr(book.item, 'LargeImage'):
+                        booklist[i].append(removeNonAscii(book.item.LargeImage.URL.__str__()))
+                    else:
+                        booklist[i].append("-")
+                    if hasattr(book.item, 'DetailPageURL'):
+                        booklist[i].append(removeNonAscii(book.item.DetailPageURL.__str__()))
+                    else:
+                        booklist[i].append("-")
+                    i+=1
+                    print book.item.__dict__
             else:
-                return dict(title = 'AmazoneBooks',renderer=FormRenderer(form))
-            return dict(title = request.POST['text'],renderer=FormRenderer(form),booklist=booklist)
+                return dict(title = 'AmazoneBooks',renderer=FormRenderer(form),booklist=[])
+            return dict(title = request.POST['text'],renderer=FormRenderer(form),booklist=booklist,css_url= './templates/style.css')
         else:
             pass
-    except NoExactMatchesFound:
+    #except NoExactMatchesFound:
  	return Response("ERROR. No such item")
-            
+
+
+
+
